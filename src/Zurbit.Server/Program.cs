adb8972467f917be4e8aa2a396c7743161412c21
Abdulla Qeblawi <abdqeb@gmail.com>
2025-10-29 23:09:57 -0400

    Testing committing to a new branch

--- src/Zurbit.Server/Program.cs
+++ src/Zurbit.Server/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
